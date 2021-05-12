8b90e4fbf06c87cb2dc1608bfd808e55a63e5d04
Pedram, Scott (206654614) <scott.pedram@nbcuni.com>
2021-05-12 14:12:27 -0600

    Added UpdateLDAPserver function

--- purecert.ps1
+++ purecert.ps1
@@ -1,10 +1,19 @@
 # $Creds = Get-Credential
-$arrays = ("uspotpure01", "usbncpure03")
+$arrays = ("ussdipure01")
+$LDAPServerAndPort = ("duoproxy.inbcu.com:2389")
+function UpdateLDAPserver {
+	param (
+		$LDAPserver
+	)
+	Invoke-Pfa2CLICommand -EndPoint $array -Credential $Creds -CommandText "pureds setattr --uri ldaps://${LDAPserver}"
+}
+
 foreach ($array in $arrays) {
 	Write-Host $array
 	$CertValue = (Invoke-Pfa2CLICommand -EndPoint $array -Credential $Creds -CommandText "purecert list --csv")
 	if ($CertValue -like '*NBCU*') {
-		Write-Host "NBCU cert present, no action required"
+		Write-Host "NBCU cert present, clear to proceed"
+		UpdateLDAPserver $LDAPServerAndPort
 	}
 	else {
 		Write-Host "No NBCU Certificate present!!!"
